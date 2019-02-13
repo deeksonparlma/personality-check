8ef4079f69a6c4fc3372c1c4597eea2afa833af7
Dickson <dicksonparlma@gmail.com>
2019-02-13 08:13:30 +0300

    styling

--- personality.js
+++ personality.js
@@ -4,25 +4,40 @@ var user=name;
 function personality(){
   var animal=prompt("Do you prefer getting a dog or a cat");
   var type=prompt("Are you an introvert or an extrovert");
-  var anim=animal;
-  var trait=type;
-  if(animal=="dog" && type==="introvert"){
+  var birds=prompt("Which bird do you like? dove or eagle?")
+  var anim=animal.toLowerCase();
+  var trait=type.toLowerCase();
+  var bird= birds.toLowerCase();
+  if(animal=="dog" && type==="introvert" && birds==="dove"){
      alert(name+" "+"Your personality is at 60%");
   }
-  else if(animal==="dog" && type==="extrovert"){
-     alert(name+" "+"Your personality is at 80%");
+  else if(animal==="dog" && type==="introvert" && bird==="eagle"){
+     alert(name+" "+"Your personality is at 65%");
   }
-  else if(animal==="cat" && type==="introvert"){
-     alert(name+" "+"Your personality is at 94%");
+  else if(animal==="dog" && type==="extrovert" && bird==="dove"){
+     alert(name+" "+"Your personality is at 79%");
   }
-  else if(animal==="cat" && type==="extrovert"){
-     alert(name+" "+"Your personality is 75%");
+  else if(animal==="dog" && type==="extrovert" && bird==="eagle"){
+     alert(name+" "+"Your personality is at 62%");
+  }
+  else if(animal==="cat" && type==="introvert" && bird==="eagle"){
+     alert(name+" "+"Your personality is at 74%");
+  }
+  else if(animal==="cat" && type==="introvert" && bird==="dove"){
+     alert(name+" "+"Your personality is 73%");
+   }
+  else if(animal==="cat" && type==="extrovert" && bird==="eagle"){
+        alert(name+" "+"Your personality is 79%");
+  }
+  else if(animal==="cat" && type==="extrovert" && bird==="dove"){
+        alert(name+" "+"Your personality is 75%");
   }
   else {
     alert("Details not captured,check your answers.Observe small letters");
   }
   document.getElementById("animal").innerHTML=anim;
   document.getElementById("type").innerHTML=trait;
-  document.getElementById("user").innerHTML=user;
   document.getElementById("us").innerHTML=user;
+  document.getElementById("user").innerHTML=user;
+  document.getElementById("bird").innerHTML=bird;
 }
